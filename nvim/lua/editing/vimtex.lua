d03ea68aa89da153db71be42b87ac50a3bf1d480
Kiran McLernon <kiranmclernon@Kirans-MacBook-Pro.local>
2023-11-10 14:13:19 +1100

    setting prioirty to zero seems to work well

--- nvim/lua/editing/vimtex.lua
+++ nvim/lua/editing/vimtex.lua
@@ -1,6 +1,9 @@
 return {
     "https://github.com/lervag/vimtex",
+    lazy = false,
+    priority = 0,
     config = function ()
+        vim.g.vimtex_compiler_method = 'latexmk'
         vim.g.vimtex_view_method = 'skim'
         vim.g.vimtex_view_skim_sync = 1
         vim.g.vimtex_view_skim_activate = 1
@@ -11,7 +14,6 @@ return {
             desc = "Disable tree sitter",
             callback = function ()
                 vim.cmd("TSBufDisable highlight")
-                vim.g.vimtex_compiler_method = 'latexmk'
                 vim.opt.conceallevel = 2
                 vim.g.tex_conceal = "abdmg"
                 vim.g.vimtex_compiler_latexmk = {
