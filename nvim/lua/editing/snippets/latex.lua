cf962c9e9d6bfe4821aa89bed5a7b3a071455ff4
Kiran McLernon <kiranmclernon@Kirans-MacBook-Pro.local>
2023-11-13 10:35:23 +1100

    snippets

--- nvim/lua/editing/snippets/latex.lua
+++ nvim/lua/editing/snippets/latex.lua
@@ -2,11 +2,9 @@ local ls = require("luasnip")
 local  s= ls.snippet
 local  f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
-local t = ls.text_node
 local i = ls.insert_node
 local extras = require("luasnip.extras")
 local fmta = require("luasnip.extras.fmt").fmta
-local l = extras.lambda
 local rep = extras.rep
 
 local function math()
@@ -16,38 +14,63 @@ end
 
 ls.add_snippets(nil, {
     all = {
-        s("beg", fmt([[
-            \begin{<>}
-            <>
-            \end{<>}]],
-            {i(1), i(0), rep(1)}, -- repeat node 1
-            {delimiters='<>'}
-        )),
-
-        s({ trig='it', name='itemize', dscr='dscr'},
-        fmt([[
-        \begin{itemize}
-            \item <>
-        \end{itemize}
-        ]],
-        { i(1) },
-        { delimiters='<>' }
-        )),
-
-        s({ trig='se', name='iset', dscr='dscr'},
-        fmt([[\{ <> \}]],
-        { i(0)},
-        { delimiters='<>' }),
-        {condition = math}),
         s(
-            {trig = '([%a%)%]%}])00', regTrig = true, wordTrig = false, snippetType="autosnippet"},
-            fmta(
-                "<>_{<>}",
-                {
-                    f( function(_, snip) return snip.captures[1] end ),
-                    t("0")
-                }
+            "beg", 
+            fmt(
+                [[
+                \begin{<>}
+                <>
+                \end{<>}]],
+                {i(1), i(0), rep(1)}, -- repeat node 1
+                {delimiters='<>'}
             )
+        ),
+
+        s(
+            "para",
+            fmt(
+                [[\paragraph{<>}]],
+                {i(1)},
+                {delimiters='<>'}
+            )
+        ),
+
+        s(
+            "mi",
+            fmt(
+                "$ <> $",
+                {i(1)},
+                {delimiters='<>'}
+            )
+        ),
+
+        s(
+            "md",
+            fmt(
+                [[ \[ <> \] ]],
+                {i(1)},
+                {delimiters='<>'}
+            )
+        ),
+
+        s(
+            { trig='it', name='itemize', dscr='dscr'},
+            fmt(
+                [[
+                \begin{itemize}
+                    \item <>
+                \end{itemize}
+                ]],
+                { i(1) },
+                { delimiters='<>' }
+            )
+        ),
+        s(
+            { trig='se', name='iset', dscr='dscr'},
+            fmt([[\{ <> \}]],
+            { i(0)},
+            { delimiters='<>' }),
+            { condition = math }
         ),
         s(
             { trig='([%a])_(%d%d)', regTrig = true, wordTrig = false, snippetType = "autosnippet"},
@@ -57,7 +80,8 @@ ls.add_snippets(nil, {
                     f( function (_, snip) return snip.captures[1] end),
                     f( function (_, snip) return snip.captures[2] end)
                 }
-            )
+            ),
+            {condition = math}
         ),
         s(
             { trig='([%a])(%d)', regTrig = true, wordTrig = false, snippetType = "autosnippet"},
@@ -67,8 +91,10 @@ ls.add_snippets(nil, {
                     f( function (_, snip) return snip.captures[1] end),
                     f( function (_, snip) return snip.captures[2] end)
                 }
-            )
+            ),
+            { condition = math }
         ),
+
     }
 })
 
