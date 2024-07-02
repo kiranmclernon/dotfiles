66a79a55bb893d903452029e96f0020d8d81945b
Kiran McLernon <kiranmclernon@Kirans-MBP.modem>
2024-07-02 17:09:23 +0800

    dapui working

--- .config/nvim/lua/debugging/init.lua
+++ .config/nvim/lua/debugging/init.lua
@@ -1,29 +1,43 @@
+local function get_package_path(package)
+    local mason_package_path = vim.env.MASON .. "/packages/"
+    local path = mason_package_path .. package
+    local file = io.open(path, "r")
+    if file then
+        file:close()
+        return path
+    else
+        error("cant find " .. path)
+    end
+end
 
 return {
     "mfussenegger/nvim-dap",
     dependencies = {
         {
+            lazy = false,
             "rcarriga/nvim-dap-ui",
             dependencies = "nvim-neotest/nvim-nio",
         },
         "mfussenegger/nvim-dap-python",
     },
     config = function()
-        local debugpy_venv = "/usr/local/Caskroom/miniconda/base/envs/debug/bin/python"
+        local debugpy_path = get_package_path("debugpy")
+        local debugpy_venv = debugpy_path .. "/venv/bin/python"
         require("dap-python").setup(debugpy_venv)
         local dap, dapui = require("dap"), require("dapui")
-        -- dap.listeners.before.attach.dapui_config = function()
-        --     dapui.open()
-        -- end
-        -- dap.listeners.before.launch.dapui_config = function()
-        --     dapui.open()
-        -- end
-        -- dap.listeners.before.event_terminated.dapui_config = function()
-        --     dapui.close()
-        -- end
-        -- dap.listeners.before.event_exited.dapui_config = function()
-        --     dapui.close()
-        -- end
+        dapui.setup()
         vim.keymap.set("n", "<leader>dc", dap.continue)
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
     end,
 }
